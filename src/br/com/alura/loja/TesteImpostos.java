47f9be69e07f2729648d0ccb3e765c9d8032eb93
Hakney <hakney.lima@sme.fortaleza.ce.gov.br>
2023-01-24 16:27:20 -0300

    Iniciando estudos com o padrão CHAIN OF RESPONSIBILITY

--- src/br/com/alura/loja/TesteImpostos.java
+++ src/br/com/alura/loja/TesteImpostos.java
@@ -6,12 +6,15 @@ import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
-public class TestesImpostos {
+public class TesteImpostos {
 
 	public static void main(String[] args) {
-		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
 		CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
 		System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS()));
+		
+		
+		
 	}
 
 }
