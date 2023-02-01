2b747a5f68d496ddb7dc4bb457d64b091ad9230d
Hakney <hakney.lima@sme.fortaleza.ce.gov.br>
2023-02-01 10:20:57 -0300

    Pattern Decorator

--- src/br/com/alura/loja/TesteImpostos.java
+++ src/br/com/alura/loja/TesteImpostos.java
@@ -3,6 +3,7 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
 import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
@@ -10,8 +11,9 @@ public class TesteImpostos {
 
 	public static void main(String[] args) {
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
-		CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
-		System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS()));
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		
+		System.out.println(calculadora.calcular(orcamento, new ISS(null)));
 		
 		
 		
